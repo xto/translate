90e523aee133eae63f21459a06b2c3c692d356a9
Xavier Tô <xavier.to@ubisoft.com>
2013-09-23 09:23:53 -0400

    change the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+Translate::Engine.routes.draw do
   get 'translate' => 'translate#index', :as => :translate_list
   post 'translate' => 'translate#translate', :as => :translate
   get 'translate/reload' => 'translate#reload', :as => :translate_reload
